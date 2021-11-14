cbb6e0838f2d2a5ecb7a7ee4bd42edc73b31222b
ChingCdesu <chingc.ccmail@gmail.com>
2021-11-14 16:42:20 +0800

    feat: 更新protocol协议逻辑

--- src/main/app/protocol.ts
+++ src/main/app/protocol.ts
@@ -1,7 +1,20 @@
-import { BrowserWindow, protocol } from "electron"
+import { BrowserWindow, protocol, app } from "electron"
+import path from 'path'
 
-export const registerSayoProtocol = (window: BrowserWindow | null): void => {
-  protocol.registerHttpProtocol('sayo', (request, cb) => {
-    window?.webContents.send('link-beatmap', request.url)
-  })
+const agreement = 'sayo'
+
+export const registerSayoProtocol = (): void => {
+  let isSet = false // 是否注册成功
+
+  app.removeAsDefaultProtocolClient(agreement) // 每次运行都删除自定义协议 然后再重新注册
+  // 开发模式下在windows运行需要做兼容
+  if (process.env.NODE_ENV === 'development' && process.platform === 'win32') {
+    // 设置electron.exe 和 app的路径
+    isSet = app.setAsDefaultProtocolClient(agreement, process.execPath, [
+      path.resolve(process.argv[1]),
+    ])
+  } else {
+    isSet = app.setAsDefaultProtocolClient(agreement)
+  }
+  console.log('是否注册成功', isSet)
 }
